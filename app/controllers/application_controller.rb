e01e00fb92d31ccf041f19e4de601bb16c4682cb
adebadayo <adebayanen@gmail.com>
2021-06-03 11:03:41 +0900

    post google chat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::API
+  require 'net/http'
 end
---
+++ app/models/google_chat.rb
@@ -0,0 +1,19 @@
+class GoogleChat < ApplicationRecord
+  require 'net/http'
+  require 'httpclient'
+
+  include ActiveModel::Model
+
+  GOOGLE_CHAT_SPACE = ENV['GOOGLE_CHAT_SPACE']
+  GOOGLE_CHAT_KEY = ENV['GOOGLE_CHAT_KEY']
+  GOOGLE_CHAT_TOKEN = ENV['GOOGLE_CHAT_TOKEN']
+
+  def self.post(body)
+    uri = URI.parse("https://chat.googleapis.com/v1/spaces/#{GOOGLE_CHAT_SPACE}/messages?key=#{GOOGLE_CHAT_KEY}&token=#{GOOGLE_CHAT_TOKEN}")
+    headers = { "Content-Type" => "application/json; charset=UTF-8" }
+    post_params = { text: body }
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.scheme === "https"
+    http.post uri, post_params.to_json, headers
+  end
+end
