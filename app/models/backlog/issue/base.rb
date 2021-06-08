8fe2fd853f86b1bf7b3c0f7af42da4e227c1710c
adebadayo <adebayanen@gmail.com>
2021-06-08 10:11:10 +0900

    refactoring

--- app/models/backlog/issue/base.rb
+++ app/models/backlog/issue/base.rb
@@ -1,8 +1,12 @@
 class Backlog::Issue::Base < Backlog::Base
-  def self.fetch_issues(opt = {})
+  def self.fetch(opt = {})
     get_params = "count=100&issueTypeId[]=#{opt[:backlog_issue_type_id]}&projectId[]=#{BACKLOG_PRJ_ID}&statusId[]=1&statusId[]=2&statusId[]=3&apiKey=#{BACKLOG_API_KEY}"
     uri = URI.parse("https://#{BACKLOG_SPACE_ID}.backlog.jp/api/v2/issues?#{get_params}")
     response = Net::HTTP.get(uri)
     JSON.parse(response)
+  end
+
+  def self.list_url(opt = {})
+    "https://#{BACKLOG_SPACE_ID}.backlog.jp/find/#{BACKLOG_PRJ_NAME}?issueTypeId=#{opt[:backlog_issue_type_id]}&statusId=1&statusId=2&statusId=3&sort=PRIORITY&order=true"
   end
 end
--- app/models/backlog/issue/bug.rb
+++ app/models/backlog/issue/bug.rb
@@ -1,11 +1,9 @@
 class Backlog::Issue::Bug < Backlog::Issue::Base
-  def self.list_url
-    "https://#{BACKLOG_SPACE_ID}.backlog.jp/find/#{BACKLOG_PRJ_NAME}?issueTypeId=#{BACKLOG_ISSUE_TYPE_ID}&statusId=1&statusId=2&statusId=3&sort=PRIORITY&order=true"
+  def self.fetch
+    super(backlog_issue_type_id: BACKLOG_ISSUE_TYPE_ID)
   end
 
-  def self.fetch
-    fetch_issues(
-      backlog_issue_type_id: BACKLOG_ISSUE_TYPE_ID
-    )
+  def self.list_url
+    super(backlog_issue_type_id: BACKLOG_ISSUE_TYPE_ID)
   end
 end
