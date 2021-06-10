f3145ce584f3af1888b8c3e212f069e0ab87130d
adebadayo <adebayanen@gmail.com>
2021-06-10 11:42:55 +0900

    add uri util module

---
+++ app/models/concerns/uri_util.rb
@@ -0,0 +1,39 @@
+module UriUtil
+  GOOGLE_CHAT_SPACE = ENV['GOOGLE_CHAT_SPACE']
+  GOOGLE_CHAT_KEY = ENV['GOOGLE_CHAT_KEY']
+  GOOGLE_CHAT_TOKEN = ENV['GOOGLE_CHAT_TOKEN']
+  BACKLOG_PROJECT_NAME = ENV['BACKLOG_PROJECT_NAME']
+  BACKLOG_SPACE_ID = ENV['BACKLOG_SPACE_ID']
+
+  private_constant :GOOGLE_CHAT_SPACE
+  private_constant :GOOGLE_CHAT_KEY
+  private_constant :GOOGLE_CHAT_TOKEN
+  private_constant :BACKLOG_PROJECT_NAME
+  private_constant :BACKLOG_SPACE_ID
+
+  # Backlog
+  def self.backlog_domain
+    "#{BACKLOG_SPACE_ID}.backlog.jp"
+  end
+
+  def self.backlog_url
+    "https://#{backlog_domain}"
+  end
+
+  def self.backlog_view_url(key_id)
+    "#{backlog_url}/view/#{BACKLOG_PROJECT_NAME}-#{key_id}"
+  end
+
+  # Google Chat
+  def self.google_chat_domain
+    "chat.googleapis.com"
+  end
+
+  def self.google_chat_url
+    "https://#{google_chat_domain}"
+  end
+
+  def self.google_chat_msg_post_url
+    "#{google_chat_url}/v1/spaces/#{GOOGLE_CHAT_SPACE}/messages?key=#{GOOGLE_CHAT_KEY}&token=#{GOOGLE_CHAT_TOKEN}"
+  end
+end
--- app/models/google_chat.rb
+++ app/models/google_chat.rb
@@ -2,16 +2,11 @@ class GoogleChat < ApplicationRecord
   require 'net/http'
 
   include ActiveModel::Model
-
-  GOOGLE_CHAT_SPACE = ENV['GOOGLE_CHAT_SPACE']
-  GOOGLE_CHAT_KEY = ENV['GOOGLE_CHAT_KEY']
-  GOOGLE_CHAT_TOKEN = ENV['GOOGLE_CHAT_TOKEN']
-  BACKLOG_PROJECT_NAME = ENV['BACKLOG_PROJECT_NAME']
-  BACKLOG_SPACE_ID = ENV['BACKLOG_SPACE_ID']
+  include UriUtil
 
   class << self
     def post(body)
-      uri = URI.parse("https://chat.googleapis.com/v1/spaces/#{GOOGLE_CHAT_SPACE}/messages?key=#{GOOGLE_CHAT_KEY}&token=#{GOOGLE_CHAT_TOKEN}")
+      uri = URI.parse(UriUtil.google_chat_msg_post_url)
       headers = { "Content-Type" => "application/json; charset=UTF-8" }
       post_params = { text: body }
       http = Net::HTTP.new(uri.host, uri.port)
@@ -23,11 +18,11 @@ class GoogleChat < ApplicationRecord
     # @opt [Hash] status
     # @opt [Hash] priority
     def body(contents = {})
-      summary = contents[:summary]
+      summary     = contents[:summary]
       status_name = contents[:status][:name]
-      comment = contents[:comment]
-      key_id = contents[:key_id]
-      issue_url = "https://#{BACKLOG_SPACE_ID}.backlog.jp/view/#{BACKLOG_PROJECT_NAME}-#{key_id}"
+      comment     = contents[:comment]
+      key_id      = contents[:key_id]
+      issue_url   = UriUtil.backlog_view_url(key_id)
       body =<<~"TEXT"
         課題「#{summary}」のステータスが「#{status_name}」に更新されました。
         #{issue_url}
