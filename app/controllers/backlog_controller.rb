c630eff8e942644fe7f762e900d8195adc0198b9
adebadayo <adebayanen@gmail.com>
2021-06-11 12:41:15 +0900

    bug fix

--- app/controllers/backlog_controller.rb
+++ app/controllers/backlog_controller.rb
@@ -7,7 +7,7 @@ class BacklogController < ApplicationController
 
   def post_to_google_chat
     content  = params.require(:content).permit!
-    changes = content[:changes].first # 配列の最初の要素にある
+    changes = content[:changes]&.first || {} # 配列の最初の要素にある
 
     if changes[:field] == "status" && changes[:new_value] == "4"
       # 完了ステータスに更新された時だけチャットに通知
