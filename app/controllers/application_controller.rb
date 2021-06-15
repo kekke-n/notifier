43a51c19ab05359b9b433fadc4a7ccfe68b80071
adebadayo <adebayanen@gmail.com>
2021-06-15 17:53:12 +0900

    validate token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::API
   require 'net/http'
 
+  before_action :valid_api_token?
+
+  def valid_api_token?
+    unless params[:token] == ENV['API_TOKEN']
+      render status: :unauthorized
+    end
+  end
+
   def format_params
     tmp_params = params.clone
     JSON.pretty_generate(tmp_params.permit!.to_h)
