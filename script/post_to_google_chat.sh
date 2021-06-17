c67078649d9761fbe90f84fe8fff3adab4b06352
adebadayo <adebayanen@gmail.com>
2021-06-17 09:30:08 +0900

    テスト用shellスクリプトをリファクタリング

--- script/post_to_google_chat.sh
+++ script/post_to_google_chat.sh
@@ -1,10 +1,9 @@
 # 環境変数読み込み
 source .config
-source $ROOT_PATH/.env
 
 # jsonデータ読み込み
 PARAMS=`cat update_issue.json`
 
-curl -X POST localhost:3008/backlog/post_to_google_chat \
+curl -X POST localhost:3008/backlog/post_to_google_chat?token=$API_TOKEN \
 -H 'Content-Type: application/json;' \
--d $PARAMS
+-d "$PARAMS"
